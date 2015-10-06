33fe71e428f4140a17bc70f72af4715abacfe5a0
David Ding <davidyding19@gmail.com>
2015-10-06 17:22:54 -0400

    grid working now

--- Backend/displayTarget.m
+++ Backend/displayTarget.m
@@ -2,7 +2,7 @@ function imageHandle = displayTarget(targ, axesHandle)
     %Given the target matrix, display the target
 
     alpha = ~targ;   %creates the alpha matrix for transparency
-    imageHandle = image([0 1], [0 1], targ, 'Parent', axesHandle);
+    imageHandle = image([0.05 0.95], [0.05 0.95], targ, 'Parent', axesHandle);
     set(imageHandle, 'AlphaData', alpha);
     colormap('gray');
     axis equal;
